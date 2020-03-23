9169bae7cefd8322c9649fad38f5923a96bcb605
kodama96 <g00338592@gmit.ie>
2020-03-23 14:59:27 +0000

    have a very basic drumkit with sounds when hit working

---
+++ Project/Assets/cylinder.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cylinder : MonoBehaviour {
+
+    public AudioSource tickSource;
+
+	// Use this for initialization
+	void Start () {
+        tickSource = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnCollisionEnter (Collision collision)
+    {
+        tickSource.Play();
+    }
+}
