d7e61d45a9c9477f3b4489f1b0d0389337fda8b1
kodama96 <g00338592@gmit.ie>
2020-03-24 18:51:28 +0000

    Full drum kit working

---
+++ Project/Assets/PlayerSound.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSound : MonoBehaviour {
+
+    public AudioSource source;
+	// Use this for initialization
+	void Start () {
+        source = GetComponent<AudioSource>();
+	}
+	
+	 void OnTriggerEnter(Collider other)
+    {
+        // if(other.tag == "DrumStickHead")
+        //{
+        // changes the audio pitch of drums each time their hit
+        source.pitch = Random.Range(0.8f, 1.2f);
+        // source.volume = other.gameObject.GetComponent<TrackSpeed>().speed;
+        source.Play();
+        //}
+    }
+}
---
+++ Project/Assets/TrackSpeed.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrackSpeed : MonoBehaviour {
+
+    private Vector3 lastPosition;
+    public float speed;
+	// Use this for initialization
+	void Start () {
+        lastPosition = transform.position;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        // Calculate the speed of the drum stick
+		speed = (((transform.position = lastPosition).magnitude) / Time.deltaTime);
+        lastPosition = transform.position;
+	}
+}
