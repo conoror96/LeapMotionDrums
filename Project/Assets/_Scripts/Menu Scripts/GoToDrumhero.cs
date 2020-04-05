2feac79c23fc7d538162d0edaf27685dc1010f44
kodama96 <g00338592@gmit.ie>
2020-04-05 20:18:13 +0100

    menu system wired up and created new difficulty levels

---
+++ Project/Assets/_Scripts/Menu Scripts/GoToDrumhero.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GoToDrumhero : MonoBehaviour {
+
+
+    void OnTriggerEnter(Collider other)
+    {
+        SceneManager.LoadScene("drumhero");
+    }
+
+
+}
---
+++ Project/Assets/_Scripts/Menu Scripts/GoToFreestyle.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GoToFreestyle : MonoBehaviour {
+
+	
+
+    void OnTriggerEnter(Collider other)
+    {
+        SceneManager.LoadScene("Freestyle");
+    }
+
+
+    
+}
