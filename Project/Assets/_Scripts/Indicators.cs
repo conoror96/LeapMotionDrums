efbea571527d5fba4e9b899c3c70413c655c1d8d
kodama96 <g00338592@gmit.ie>
2020-04-05 13:25:18 +0100

    started working on drumhero features

---
+++ Project/Assets/_Scripts/Indicators.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Indicators : MonoBehaviour {
+
+
+    public GameObject prefab;
+
+    public string notation;
+    public float speed;
+
+    private float nextPlayTime;
+    private int currentNote;
+   
+
+
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+
+    // Update is called once per frame
+    void Update() {
+        // if (notation.Length == 0) return;
+        if (Time.time > nextPlayTime)
+        {
+            //nextPlayTime = Time.time + SpeedController.Instance.speed;
+            nextPlayTime = Time.time + speed;
+            if(notation.Substring(currentNote,1) != "0")
+            {
+                //CreateNote(notation.Substring(currentNote, 1));
+                CreateNote();
+            }
+            currentNote++;
+            if (currentNote >= notation.Length) currentNote = 0;
+        }
+    }
+   
+    void CreateNote()
+    {
+        GameObject tempGO = Instantiate(prefab, transform);
+        tempGO.transform.localPosition = new Vector3(0,0,5);
+        //if (hand == "L") tempGO.GetComponent<HandIndicator>().leftHand.SetActive(true);
+        //if (hand == "R") tempGO.GetComponent<HandIndicator>().rightHand.SetActive(true);
+        iTween.MoveTo(tempGO, iTween.Hash("position", new Vector3(0,0,0), "time", 2f, "easeType", iTween.EaseType.linear, "isLocal", true));
+        Destroy(tempGO, 2f);
+    }
+}
