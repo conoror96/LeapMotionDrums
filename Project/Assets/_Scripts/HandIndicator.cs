b0a4494cf4b3f32c6466250a4bc0df9379e39c36
kodama96 <g00338592@gmit.ie>
2020-04-05 14:52:38 +0100

    progress on drumhero screen

---
+++ Project/Assets/_Scripts/HandIndicator.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandIndicator : MonoBehaviour {
+
+    public GameObject lefthand;
+    public GameObject righthand;
+}
--- Project/Assets/_Scripts/Indicators.cs
+++ Project/Assets/_Scripts/Indicators.cs
@@ -8,7 +8,7 @@ public class Indicators : MonoBehaviour {
     public GameObject prefab;
 
     public string notation;
-    public float speed;
+
 
     private float nextPlayTime;
     private int currentNote;
@@ -23,27 +23,27 @@ public class Indicators : MonoBehaviour {
 
     // Update is called once per frame
     void Update() {
-        // if (notation.Length == 0) return;
+        if (notation.Length == 0) return;
         if (Time.time > nextPlayTime)
         {
-            //nextPlayTime = Time.time + SpeedController.Instance.speed;
-            nextPlayTime = Time.time + speed;
+            nextPlayTime = Time.time + SpeedController.Instance.speed;
+            //nextPlayTime = Time.time + speed;
             if(notation.Substring(currentNote,1) != "0")
             {
-                //CreateNote(notation.Substring(currentNote, 1));
-                CreateNote();
+                CreateNote(notation.Substring(currentNote, 1));
+                //CreateNote();
             }
             currentNote++;
             if (currentNote >= notation.Length) currentNote = 0;
         }
     }
    
-    void CreateNote()
+    void CreateNote(string hand)
     {
         GameObject tempGO = Instantiate(prefab, transform);
         tempGO.transform.localPosition = new Vector3(0,0,5);
-        //if (hand == "L") tempGO.GetComponent<HandIndicator>().leftHand.SetActive(true);
-        //if (hand == "R") tempGO.GetComponent<HandIndicator>().rightHand.SetActive(true);
+        if (hand == "L") tempGO.GetComponent<HandIndicator>().lefthand.SetActive(true);
+        if (hand == "R") tempGO.GetComponent<HandIndicator>().righthand.SetActive(true);
         iTween.MoveTo(tempGO, iTween.Hash("position", new Vector3(0,0,0), "time", 2f, "easeType", iTween.EaseType.linear, "isLocal", true));
         Destroy(tempGO, 2f);
     }
