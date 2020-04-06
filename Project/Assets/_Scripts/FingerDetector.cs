1a0f38af65bccd47df3b7afc0430847c4e417a2b
kodama96 <g00338592@gmit.ie>
2020-04-06 21:46:11 +0100

    new gestures for menu and navigation

---
+++ Project/Assets/_Scripts/FingerDetector.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class FingerDetector : MonoBehaviour {
+
+	public void Option1()
+    {
+        print("Activated");
+        SceneManager.LoadScene("freestyle");
+    }
+    public void Option2()
+    {
+        print("Activated");
+        SceneManager.LoadScene("difficultymenu");
+    }
+    public void Option3()
+    {
+        print("Activated");
+        SceneManager.LoadScene("menu");
+    }
+    public void DifficultyOption1()
+    {
+        SceneManager.LoadScene("easy");
+    }
+    public void DifficultyOption2()
+    {
+        SceneManager.LoadScene("medium");
+    }
+    public void DifficultyOption3()
+    {
+        SceneManager.LoadScene("hard");
+    }
+}
